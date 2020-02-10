61e8ff03c64e6cb709e3cae69541b11524e0e36e
Ronnie Pereira <vinniciusrp@gmail.com>
2020-02-10 21:42:40 -0200

    frontend adjustments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>OW Interactive</title>
+        <title>Laravel Events</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    OW Interactive
+                    Laravel Events
                 </div>
             </div>
         </div>
